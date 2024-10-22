024ceb3530492110cc821f6f375ffba59a8f370f
sarkar75 <131634214+sarkar75@users.noreply.github.com>
2024-10-22 11:05:37 +0530

    Add files via upload

---
+++ real_time_object_detection.py
@@ -0,0 +1,95 @@
+
+# import packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help=r"C:\Users\Shreeyansh\Desktop\real-Time-Object-Detection-With-OpenCV-master\real_time_object_detection.py")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak predictions")
+args = vars(ap.parse_args())
+
+CLASSES = [  "person","bottle","tvmonitor","mobile"]
+            
+
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+# open up the camera for a couple of seconds
+time.sleep(2.0)
+
+fps = FPS().start()
+
+
+while True:
+	
+	# vs is the VideoStream
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+	print(frame.shape) # (225, 400, 3)
+	
+	(h, w) = frame.shape[:2]
+	# Resize each frame
+	resized_image = cv2.resize(frame, (300, 300))
+	
+
+	blob = cv2.dnn.blobFromImage(resized_image, (1/127.5), (300, 300), 127.5, swapRB=True)
+	
+	net.setInput(blob) 
+	
+	predictions = net.forward()
+
+	for i in np.arange(0, predictions.shape[2]):
+		
+		confidence = predictions[0, 0, i, 2]
+		
+		if confidence > args["confidence"]:
+			
+			idx = int(predictions[0, 0, i, 2])
+   
+			box = predictions[0, 0, i, 3:7] * np.array([w, h, w, h])
+   
+			(startX, startY, endX, endY) = box.astype("int")
+
+		
+			label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+			print("Object detected:",label)
+			
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+			    COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			
+			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	
+	cv2.imshow("Frame", frame)
+
+	
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("q"):
+		break
+
+	fps.update()
+
+fps.stop()
+
+print("[INFO] Elapsed Time: {:.2f}".format(fps.elapsed()))
+print("[INFO] Approximate FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+vs.stop()
+
